ad18946a36fb1303823e1c509a3bc7501eb05a8d
DarrelFW321 <60972474+DarrelFW321@users.noreply.github.com>
2026-02-11 11:08:36 -0500

    Create 167-TwoSumIiInputArrayIsSorted

---
+++ 167-TwoSumIiInputArrayIsSorted/167-TwoSumIiInputArrayIsSorted.cpp
@@ -0,0 +1,24 @@
+// Last updated: 2/11/2026, 11:08:30 AM
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left = 0;
+        int right = numbers.size()-1;
+        vector<int> res;
+
+        while (left < right){
+            if (numbers[left] + numbers[right] == target){
+                res.push_back(left+1);
+                res.push_back(right+1);
+                return res;
+            }
+            else if (numbers[left]+numbers[right] < target){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return res;
+    }
+};
