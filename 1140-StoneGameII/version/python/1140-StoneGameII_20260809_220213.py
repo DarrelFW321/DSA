a03052a894fcd1f88cf4eec65818920ef973356b
Darrel Feiro Wihandi <60972474+DarrelFW321@users.noreply.github.com>
2026-08-09 22:06:00 -0400

    Create 1140-StoneGameII

---
+++ 1140-StoneGameII/version/python/1140-StoneGameII_20260809_220213.py
@@ -0,0 +1,27 @@
+# Last updated: 8/9/2026, 10:02:13 PM
+1class Solution:
+2    def winnerSquareGame(self, n: int) -> bool:
+3        
+4        mp = {}
+5
+6        def dp(num):
+7            if num in mp:
+8                return mp[num]
+9            if num == 0:
+10                mp[num] = False
+11                return False
+12
+13            for i in range(1, num+1):
+14                if i*i > num:
+15                    break
+16                if not dp(num-(i*i)):
+17                    mp[num] = True
+18                    return True
+19
+20            mp[num] = False
+21            return False
+22
+23        return dp(n)
+24
+25
+26            
